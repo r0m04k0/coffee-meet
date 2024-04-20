d94aa2c5f353f83280590e568427856e93478de0
de-lolec <matvey20158@mail.ru>
2024-04-20 17:15:03 +0300

    feat: change migrations (sorry laravel) added based pages in filament

---
+++ app/Filament/Resources/UserResource/Pages/ViewUser.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\Pages;
+
+use App\Filament\Resources\UserResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+use Filament\Infolists;
+use Filament\Infolists\Infolist;
+
+class ViewUser extends ViewRecord
+{
+    protected static string $resource = UserResource::class;
+
+
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\EditAction::make(),
+        ];
+    }
+
+    public function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Infolists\Components\TextEntry::make('name'),
+                Infolists\Components\TextEntry::make('email'),
+                Infolists\Components\TextEntry::make('patronymic')
+                    ->columnSpanFull(),
+            ]);
+    }
+}
--- app/Models/Meet.php
+++ app/Models/Meet.php
@@ -4,8 +4,34 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Meet extends Model
 {
     use HasFactory;
+
+    public function first_user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user1_id', 'id');
+    }
+
+    public function second_user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user2_id', 'id');
+    }
+
+    public function first_duration(): BelongsTo
+    {
+        return $this->belongsTo(Duration::class, '1duration_id', 'id');
+    }
+
+    public function second_duration(): BelongsTo
+    {
+        return $this->belongsTo(Duration::class, '2duration_id', 'id');
+    }
+
+    public function final_duration() 
+    {   
+        return $this->belongsTo(Duration::class, 'duration_id', 'id');
+    }
 }
