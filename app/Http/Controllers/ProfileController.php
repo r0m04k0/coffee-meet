6ab4e7215a215acda8e9ca79f50f4091602fa12b
de-lolec <matvey20158@mail.ru>
2024-04-21 02:23:27 +0300

    feat: change nginx conf and change profile enpoints

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
 
@@ -6,56 +6,60 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class ProfileController extends Controller
 {
-    /**
-     * Информация профиля пользователя
-     *
-     * @return JsonResponse
-     */
-    public function getProfileInfo(): JsonResponse
-    {
-        $profileData = User::where('id', Auth::user()->id)
-          ->select(
-            "id",
-            "name",
-            "surname",
-            "patronymic",
-            "email",
-            "position",
-            "departament",
-            "about",
-            "phone",
-            "telegram",
-            "is_confirmed",
-            "is_ready",
-            "date_birth",
-          )
-          ->first();
+  /**
+   * Информация профиля пользователя
+   *
+   * @return JsonResponse
+   */
+  public function getProfileInfo(): JsonResponse
+  {
+      $profileData = User::where('id', Auth::user()->id)
+        ->select(
+          "id",
+          "name",
+          "surname",
+          "patronymic",
+          "email",
+          "position",
+          "departament",
+          "about",
+          "phone",
+          "telegram",
+          "is_confirmed",
+          "is_ready",
+          "date_birth",
+        )
+        ->first();
 
-          return response()->json($profileData);
-    }
+        $years = Carbon::now()->diffInYears(Carbon::parse($profileData->date_birth));
 
-    /**
-     * Обновление данных профиля
-     *
-     * @param Request $request
-     * @return JsonResponse
-     */
-    public function updateProfile(Request $request): JsonResponse
-    {
-        $updatedData = User::where('id', Auth::user()->id)
-          ->update([
-            'email' => $request->email,
-            'position' => $request->position,
-            'departament' => $request->departament,
-            'about' => $request->about,
-            'phone' => $request->phone,
-            'telegram' => $request->telegram,
-            'date_birth' =>$request->date_birth,
-          ]);
+        $profileData->setAttribute('age', $years);
 
-        return response()->json($updatedData);
-    }
+        return response()->json($profileData);
+  }
+
+  /**
+   * Обновление данных профиля
+   *
+   * @param Request $request
+   * @return JsonResponse
+   */
+  public function updateProfile(Request $request): JsonResponse
+  {
+    $updatedData = User::where('id', Auth::user()->id)
+      ->update([
+        'position' => $request->position,
+        'departament' => $request->departament,
+        'about' => $request->about,
+        'phone' => $request->phone,
+        'telegram' => $request->telegram,
+        'date_birth' => Carbon::parse($request->date_birth)->format('Y-m-d'),
+      ]);
+
+    return response()->json($updatedData);
+  }
 }
--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ use App\Http\Controllers\UserController;
 
 Route::middleware('auth:sanctum')->group(function () {
   Route::get('/user', function (Request $request) {
-      return $request->user();
+    return $request->user();
   });
 
   Route::post('/logout', [AuthController::class, 'logout']);
