8f5ae8994073228baa02aa32cbbdece7724b6c5c
de-lolec <matvey20158@mail.ru>
2024-04-20 20:09:21 +0300

    feat: added registratioin

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request['name'],
+            'surname' => $request['surname'],
+            'patronymic' => $request['patronymic'],
+            'email' => $request['email'],
+            'password' => bcrypt($request['password']),
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+        return response([
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email|string|exists:users,email',
+            'password' => [
+                'required',
+            ],
+            'remember' => 'boolean'
+        ]);
+        $remember = $credentials['remember'] ?? false;
+        unset($credentials['remember']);
+
+        if (!Auth::attempt($credentials, $remember)) {
+            return response([
+                'error' => 'The Provided credentials are not correct'
+            ], 422);
+        }
+
+        $user = auth()->user();
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function logout()
+    {
+        $user = Auth::user();
+
+        $user->currentAccessToken()->delete();
+
+        return response([
+            'success' => true
+        ]);
+    }
+}
