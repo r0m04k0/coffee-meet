90767b8a05353fc8d77a298194836b1e889726e7
de-lolec <matvey20158@mail.ru>
2024-04-20 12:37:57 +0300

    fear: added migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,18 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
+            $table->string('patronymic');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('position');
+            $table->string('departament');
+            $table->string('about');
+            $table->string('phone');
+            $table->string('telegram');
+            $table->boolean('is_confirmed');
+            $table->boolean('is_ready');
             $table->rememberToken();
             $table->timestamps();
         });
